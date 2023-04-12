6106770eac2c56a929eaba4655d492146d09962d
Ihor Smyk <ismyk256@gmail.com>
2023-04-12 17:42:13 +0300

    aspects commit

---
+++ src/main/java/com/dotcat/springhw/entity/Product.java
@@ -0,0 +1,37 @@
+package com.dotcat.springhw.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "Decimal(7,2)")
+    private Double price;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "products")
+    private Set<UserOrder> orders = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", orders=" + orders +
+                '}';
+    }
+}
